c5ee0ce44cb4a16ed535c9dc71a8e0a508f3b51f
ryan.ql <ryan.ql@webi.com.cn>
2016-01-08 10:23:19 +0800

    add files

---
+++ H5/js/WorkerTest.js
@@ -0,0 +1,11 @@
+﻿function getPI(n) {
+    var a = 0;
+    for (var i = 0; i < n; i++) {
+        a = a + 4 * Math.pow(-1, i) / (2 * i + 1);
+    }
+    return a;
+}
+
+onmessage = function (e) {
+    postMessage(getPI(e.data));
+}
